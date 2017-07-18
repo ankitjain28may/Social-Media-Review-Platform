5b48f3bf1dfe938e49470d15ffca0b79164386c6
ankitjain28may <ankitjain28may77@gmail.com>
2017-07-19 00:31:07 +0530

    Added seeder #1

---
+++ database/seeds/GroupTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Group;
+
+class GroupTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $data = [
+            [
+                'name' => 'User',
+                'slug' => 'user',
+                'is_admin' =>  0,
+                'level' => 3
+            ],
+            [
+                'name' => 'Admin',
+                'slug' => 'admin',
+                'is_admin' =>  1
+            ]
+        ];
+
+        Group::insert($data);
+    }
+}
